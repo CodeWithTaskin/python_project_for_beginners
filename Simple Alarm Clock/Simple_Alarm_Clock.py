2346762681bdaf6e37028ae2f88246ab63088e6e
Farjhan Ahmed <nkftt58346@gmail.com>
2024-05-30 15:00:00 +0600

    #

--- Simple Alarm Clock/Simple_Alarm_Clock.py
+++ Simple Alarm Clock/Simple_Alarm_Clock.py
@@ -1,23 +1,21 @@
 import datetime as dt
+from time import sleep
 from playsound import playsound
 
 
 Time = int(input("Enter your alarm time(hour): "))
 Time2 = int(input("Enter your alarm time(minite): "))
 AMorPM = input("AM or PM? : ")
-alarmTime = (f"{Time}:{Time2}{AMorPM.upper()}")
+if Time < 10:
+    alarmTime = (f"0{Time}:{Time2}{AMorPM.upper()}")
+else:
+    alarmTime = (f"{Time}:{Time2}{AMorPM.upper()}")
 
 while True:
     now=dt.datetime.now()
-    t = now.strftime("%H:%M%p")
-    if t == alarmTime:
+    t = now.strftime("%I:%M%p")
+    if  t == alarmTime :
         print("Weak up!!!!")
         playsound('E:/python_project/python_project_for_beginners/Simple Alarm Clock/alarm-clock-short-6402.mp3')
         break
         
-
-
-
-
-
-
