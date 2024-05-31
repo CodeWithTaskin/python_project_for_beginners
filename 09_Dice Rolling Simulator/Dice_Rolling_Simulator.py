cc3c4aededf75a4cf39185ad3493123382350f16
Farjhan Ahmed <nkftt58346@gmail.com>
2024-05-31 18:10:07 +0600

    #

---
+++ 09_Dice Rolling Simulator/Dice_Rolling_Simulator.py
@@ -0,0 +1,59 @@
+import random
+
+DICE_ART = {
+    1: (
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘",
+    ),
+    2: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    3: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    4: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    5: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    6: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+}
+while True:
+    dice_roll1 = random.randint(1,6)
+    dice_roll2 = random.randint(1,6)
+    num_of_roll = input("Do you want to roll press enter ")
+    try:
+        if num_of_roll.lower() == "":
+            print("\n".join(DICE_ART[dice_roll1]))
+            print("\n".join(DICE_ART[dice_roll2]))
+        else:
+            break
+    except:
+        print("Invalid Input")
+
