acf99b04889fdde0b52f0d5e1cc853eccd4d1e0a
Farjhan Ahmed <nkftt58346@gmail.com>
2024-06-03 16:45:31 +0600

    #

--- 02_NumberGuessingGame/number_guessing_game.py
+++ 02_NumberGuessingGame/number_guessing_game.py
@@ -1,15 +1,39 @@
 import random as r
-randomGuess = r.randint(1,20)
+randomGuess = r.randint(1,100)
 print(randomGuess)
+life = 5
+life1 = 10
+
+
+userChoice = input("Choose a difficulty. Type 'easy' or 'hard': ")
 while True:
-    try:
-        userGuess = int(input("Enter a number between 1-20: "))
-        if userGuess > randomGuess:
-            print("wrong! Too Higher....")
-        elif userGuess < randomGuess:
-            print("Wront! Too Lower....")
-        else:
-            print(f"""That's right randon guess is ==> {randomGuess} <== and Your guess is ==> {userGuess} <==!!!""")
+    if userChoice.lower() == "easy":
+        user = int(input("Make a Guess: "))
+        if user > randomGuess:
+            life1 -= 1
+            print("Too high...\nGuess again.")
+            print(f"You have {life1} attempts remaining to guess the number.")
+        elif user < randomGuess:
+            life1 -= 1
+            print("Too low...\nGuess again.")
+            print(f"You have {life1} attempts remaining to guess the number.")
+        elif user == randomGuess:
+            print(f"You got it. The answer was {randomGuess}")
             break
-    except:
-        print("Invalid Input")
+    else:
+        user = int(input("Make a Guess: "))
+        if user > randomGuess:
+            life -= 1
+            print("Too high...\nGuess again.")
+            print(f"You have {life} attempts remaining to guess the number.")
+        elif user < randomGuess:
+            life -= 1
+            print("Too low...\nGuess again.")
+            print(f"You have {life} attempts remaining to guess the number.")
+        elif user == randomGuess:
+            print(f"You got it. The answer was {randomGuess}")
+            break
+    if life1 == 0 or life == 0:
+        print("You lost!!!!")  
+        break 
+        
